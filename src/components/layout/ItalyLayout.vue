b17b00dcbc77399affaf09fb1aac9a59d76106d2
Lukas Pajma <extpajma@dr-max.global>
2026-07-01 09:04:56 +0200

    feat(italy): always ensure SKU 100200309 is preset as excluded on mount

--- src/components/layout/ItalyLayout.vue
+++ src/components/layout/ItalyLayout.vue
@@ -9,10 +9,18 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import AppTopBar from './AppTopBar.vue'
 import AppSidebar from './AppSidebar.vue'
 import MaxikHelper from '../MaxikHelper.vue'
+import { useSettingsStore } from '../../stores/settings'
 
 const drawerOpen = ref(true)
+const settings = useSettingsStore()
+
+onMounted(() => {
+  if (!settings.excludedSkus.includes('100200309')) {
+    settings.save({ excludedSkus: [...settings.excludedSkus, '100200309'] })
+  }
+})
 </script>
